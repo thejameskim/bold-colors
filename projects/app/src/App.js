a316b5a5687e948fdaa3c9bd52cbb18be7618e40
James Kim <jameskim.1997@yahoo.com>
2020-02-22 12:18:47 -0800

    set up project

---
+++ projects/app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { ObjectRecognition } from './components/ObjectRecognition'
+
+function App() {
+  return (
+    <ObjectRecognition/>
+  );
+}
+
+export default App;
