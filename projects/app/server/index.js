dd0d86b5435334453178529f0f578f373d5921c1
James Kim <jameskim.1997@yahoo.com>
2020-03-02 07:51:16 -0800

    add opencv.js

--- projects/app/server/index.js
+++ projects/app/server/index.js
@@ -1,2 +1,9 @@
-const express = require("express");
-
+// Define a global variable 'Module' with a method 'onRuntimeInitialized':
+Module = {
+  onRuntimeInitialized() {
+    // this is our application:
+    console.log(cv.getBuildInformation())
+  }
+}
+// Load 'opencv.js' assigning the value to the global variable 'cv'
+cv = require('./opencv.js')
