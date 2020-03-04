bb855ac2841ff9a2881b2690cc22cb4fd76c4b85
James Kim <jameskim.1997@yahoo.com>
2020-03-04 14:41:04 -0800

    main functionality

--- projects/app/server/index.js
+++ projects/app/server/index.js
@@ -1,6 +1,5 @@
-function onCvLoaded () {
-  console.log('cv', cv);
-  cv.onRuntimeInitialized = onReady;
+function onCvLoaded() {
+    cv.onRuntimeInitialized = onReady;
 }
 const video = document.getElementById('videoInput');
 const width = 320;
@@ -9,11 +8,50 @@ const FPS = 30;
 let stream;
 let streaming = false;
 
-function onReady () {
+const HSV_RANGES = {
+    'yellow': {
+        'lower': [20, 100, 100, 0],
+        'upper': [30, 255, 255, 255]
+    },
+    'red': {
+        'lower': [],
+        'upper': []
+    },
+    'green': {
+        'lower': [50, 50, 50, 0],
+        'upper': [70, 255, 255, 255]
+    },
+    'purple': {
+        'lower': [],
+        'upper': []
+    },
+    'orange': {
+        'lower': [],
+        'upper': []
+    },
+    'black': {
+        'lower': [],
+        'upper': []
+    },
+    'blue': {
+        'lower': [110, 100, 100, 0],
+        'upper': [130, 255, 255, 255]
+    },
+    'pink': {
+        'lower': [],
+        'upper': []
+    },
+    'white': {
+        'lower': [],
+        'upper': []
+    },
+}
+
+function onReady() {
     let src;
     let dst;
+    let hsv;
     const cap = new cv.VideoCapture(video);
-
     actionBtn.addEventListener('click', () => {
         if (streaming) {
             stop();
@@ -24,22 +62,23 @@ function onReady () {
         }
     });
 
-    function start () {
+    function start() {
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then(_stream => {
-            stream = _stream;
-            console.log('stream', stream);
-            video.srcObject = stream;
-            video.play();
-            streaming = true;
-            src = new cv.Mat(height, width, cv.CV_8UC4);
-            dst = new cv.Mat(height, width, cv.CV_8UC1);
-            setTimeout(processVideo, 0)
-        })
-        .catch(err => console.log(`An error occurred: ${err}`));
+            .then(_stream => {
+                stream = _stream;
+                console.log('stream', stream);
+                video.srcObject = stream;
+                video.play();
+                streaming = true;
+                src = new cv.Mat(height, width, cv.CV_8UC4);
+                dst = new cv.Mat(height, width, cv.CV_8UC1);
+                hsv = new cv.Mat();
+                setTimeout(processVideo, 0)
+            })
+            .catch(err => console.log(`An error occurred: ${err}`));
     }
 
-    function stop () {
+    function stop() {
         if (video) {
             video.pause();
             video.srcObject = null;
@@ -50,19 +89,38 @@ function onReady () {
         streaming = false;
     }
 
-    function processVideo () {
+    function processVideo() {
         if (!streaming) {
             src.delete();
             dst.delete();
             return;
         }
         const begin = Date.now();
-        cap.read(src)
-        hsv = cv.cvtColor(src, cv.COLOR_RGB2HSV);
+        cap.read(src);
+        src.copyTo(dst);
         
+        let selectedColor = selector.value
+        cv.cvtColor(src, hsv, cv.COLOR_RGB2HSV);
+        let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), HSV_RANGES[selectedColor].lower);
+        let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), HSV_RANGES[selectedColor].upper);
+        cv.inRange(hsv, low, high, hsv);
+
+        const contours = new cv.MatVector();
+        const hierarchy = new cv.Mat();
+        cv.findContours(hsv, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
+        const contoursSize = contours.size();
+
+        for (let i = 0; i < contoursSize; ++i) {
+            let obj = contours.get(i);
+            let rect = cv.boundingRect(obj);
+            let point1 = new cv.Point(rect.x, rect.y);
+            let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
+            cv.rectangle(dst, point1, point2, [255, 0, 0, 255])
+        }
+
         cv.imshow('canvasOutput', dst);
 
-        const delay = 1000/FPS - (Date.now() - begin);
+        const delay = 1000 / FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
     }
 }
