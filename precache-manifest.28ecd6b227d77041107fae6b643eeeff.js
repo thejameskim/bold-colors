c75c3a4a851afa4a42e82f690db3399a399a084b
James <jsjkim97@gmail.com>
2021-10-06 20:28:23 -0700

    Updates

---
+++ precache-manifest.28ecd6b227d77041107fae6b643eeeff.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5a0a4a2ca3531ed696095800611322",
+    "url": "/bold-colors/index.html"
+  },
+  {
+    "revision": "d2cbdaa9e5677096dc97",
+    "url": "/bold-colors/static/css/main.f417be60.chunk.css"
+  },
+  {
+    "revision": "7ea78e602bc2d3750035",
+    "url": "/bold-colors/static/js/2.a405dcec.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/bold-colors/static/js/2.a405dcec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cbdaa9e5677096dc97",
+    "url": "/bold-colors/static/js/main.83a7ba0a.chunk.js"
+  },
+  {
+    "revision": "4bc0cbf6a6634507f088",
+    "url": "/bold-colors/static/js/runtime-main.159cb2dd.js"
+  },
+  {
+    "revision": "44d40bcba28a4d2d812a657314fcf1b9",
+    "url": "/bold-colors/static/media/allison-color.44d40bcb.png"
+  },
+  {
+    "revision": "ad47caa52719f17e0dc0bb1ad2a4df4b",
+    "url": "/bold-colors/static/media/allison-outline.ad47caa5.png"
+  },
+  {
+    "revision": "76ac542cdd111299848e1b5b9e245068",
+    "url": "/bold-colors/static/media/background-1-circles.76ac542c.png"
+  },
+  {
+    "revision": "9085675e25b9d717c8570103a19342d1",
+    "url": "/bold-colors/static/media/background-1.9085675e.png"
+  },
+  {
+    "revision": "187816f8c49d76473e8ceb359638036e",
+    "url": "/bold-colors/static/media/background-2-circles.187816f8.png"
+  },
+  {
+    "revision": "ee85b3b7a0a9b2a94763d8e4f39a91ec",
+    "url": "/bold-colors/static/media/background-2.ee85b3b7.png"
+  },
+  {
+    "revision": "c45d0855a4ec2c09e760f1f8e458a89d",
+    "url": "/bold-colors/static/media/background-3-circles.c45d0855.png"
+  },
+  {
+    "revision": "57f88ab4438c81144e227fba22334c8c",
+    "url": "/bold-colors/static/media/background-3.57f88ab4.png"
+  },
+  {
+    "revision": "c0c97b7c48706bcf4551b0cbef345381",
+    "url": "/bold-colors/static/media/elisa-color.c0c97b7c.png"
+  },
+  {
+    "revision": "5d129976dcb524bb286e2e09818ad60c",
+    "url": "/bold-colors/static/media/elisa-outline.5d129976.png"
+  },
+  {
+    "revision": "e3377cb185a41ffdb452fe72233941a6",
+    "url": "/bold-colors/static/media/iphone.e3377cb1.png"
+  },
+  {
+    "revision": "f151ddbcaab788135df07d86a59d08be",
+    "url": "/bold-colors/static/media/james-color.f151ddbc.png"
+  },
+  {
+    "revision": "ba8a5e43fc61fdd5ebaf4e5febe3ec3f",
+    "url": "/bold-colors/static/media/james-outline.ba8a5e43.png"
+  },
+  {
+    "revision": "d7aa18c56c5e0c2a2ce19a1d0702474d",
+    "url": "/bold-colors/static/media/main-image-circles.d7aa18c5.png"
+  },
+  {
+    "revision": "65e4d6b78640431f46ed4fd3066705f0",
+    "url": "/bold-colors/static/media/main-image.65e4d6b7.png"
+  },
+  {
+    "revision": "650be8ecddd930173c526607d949e2bf",
+    "url": "/bold-colors/static/media/outline-1.650be8ec.png"
+  },
+  {
+    "revision": "9625abe2bfa587e93a3dc6b011ba6652",
+    "url": "/bold-colors/static/media/outline-2.9625abe2.png"
+  },
+  {
+    "revision": "a001465365871216f7c1c3a5c445c50b",
+    "url": "/bold-colors/static/media/outline-3.a0014653.png"
+  },
+  {
+    "revision": "bc167b11f4aa5ea7cd18b1acfdcfbf62",
+    "url": "/bold-colors/static/media/outline-4.bc167b11.png"
+  },
+  {
+    "revision": "229f40a1de0e430891b98ec7451ca2c0",
+    "url": "/bold-colors/static/media/outline-5.229f40a1.png"
+  },
+  {
+    "revision": "5d8adac23ccac67b817d2060ce7f9ce1",
+    "url": "/bold-colors/static/media/outline-6.5d8adac2.png"
+  },
+  {
+    "revision": "aed490c3a736550b491c2d4dc4b6abf8",
+    "url": "/bold-colors/static/media/twentiethcentury.aed490c3.woff"
+  },
+  {
+    "revision": "e137aca71aac055d8deeea5264a3644d",
+    "url": "/bold-colors/static/media/will-color.e137aca7.png"
+  },
+  {
+    "revision": "5f25a03632d1f046aa2ea9af2afb9eb9",
+    "url": "/bold-colors/static/media/will-outline.5f25a036.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38e084a4b52ae4c808665033a6ab01d9.js"
+  "/bold-colors/precache-manifest.28ecd6b227d77041107fae6b643eeeff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bold-colors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
